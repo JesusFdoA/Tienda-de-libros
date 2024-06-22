94fe10469b196d1d4db42a67f95dc1e971ac634e
Jesus Alvis <jalvisbarcenas64@correo.unicordoba.edu.co>
2024-06-21 21:20:35 -0500

    Actualizacion de la clase utiliades

--- Tienda_Virtual_Libros/src/Utilidades/Utilidades.java
+++ Tienda_Virtual_Libros/src/Utilidades/Utilidades.java
@@ -4,6 +4,10 @@
  */
 package Utilidades;
 
+import modelos.Administrador;
+import modelos.Libro;
+import modelos.Usuario;
+
 /**
  *
  * @author Juan Mangones
@@ -11,11 +15,21 @@ package Utilidades;
 public class Utilidades {
 
     private static Utilidades instance;
+    private Administrador administrador;
+    private Usuario usuario;
+    private Libro libro;
 
-  
     private Utilidades() {
       
     }
+
+    public Utilidades(Administrador administrador, Usuario usuario, Libro libro) {
+        this.administrador = administrador;
+        this.usuario = usuario;
+        this.libro = libro;
+    }
+    
+    
 
     public static Utilidades getInstance() {
       
@@ -25,4 +39,29 @@ public class Utilidades {
         return instance;
     }
 
+    public Administrador getAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(Administrador administrador) {
+        this.administrador = administrador;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+    
+    
 }
