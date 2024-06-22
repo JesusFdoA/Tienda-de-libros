e1b6390ffbc0239ed43b117be0b89cc3f6cea557
Carlos Martinez <lagnusmac@gmail.com>
2024-06-21 20:57:04 -0500

    Creación de modelo para administrador

---
+++ Tienda_Virtual_Libros/src/modelos/Administrador.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author lagnu
+ */
+public class Administrador {
+    private String correo, contraseña;
+
+    public Administrador() {
+    }
+
+    public Administrador(String correo, String contraseña) {
+        this.correo = correo;
+        this.contraseña = contraseña;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+}
