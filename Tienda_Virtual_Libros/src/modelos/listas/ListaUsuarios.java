0fecea39be80b337d0cc1442dcb749ff932b77d2
Jesus Alvis <jalvisbarcenas64@correo.unicordoba.edu.co>
2024-06-21 22:08:55 -0500

    Creacion de la lista Usuarios

---
+++ Tienda_Virtual_Libros/src/modelos/listas/ListaUsuarios.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos.listas;
+
+import modelos.Usuario;
+
+public class ListaUsuarios {
+
+    private Usuario cab;
+
+    public ListaUsuarios() {
+    }
+
+    public Usuario getCab() {
+        return cab;
+    }
+
+    public void setCab(Usuario cab) {
+        this.cab = cab;
+    }
+    
+}
