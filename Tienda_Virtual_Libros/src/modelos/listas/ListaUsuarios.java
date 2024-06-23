ca7d7bc613d637443572dc8fe479e1d7b5834db1
Jesus Treco <jesustrecos@gmail.com>
2024-06-22 21:07:51 -0500

    Cambios en la clase de lista de usuarios

--- Tienda_Virtual_Libros/src/modelos/listas/ListaUsuarios.java
+++ Tienda_Virtual_Libros/src/modelos/listas/ListaUsuarios.java
@@ -4,21 +4,37 @@
  */
 package modelos.listas;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import modelos.Usuario;
 
 public class ListaUsuarios {
 
-    private Usuario cab;
+  private ArrayList<Usuario> listUsuarios;
 
     public ListaUsuarios() {
-    }
-
-    public Usuario getCab() {
-        return cab;
-    }
-
-    public void setCab(Usuario cab) {
-        this.cab = cab;
+        listUsuarios = new ArrayList<>();
+        Usuario usuario = new Usuario("Pedro", "Montes", 
+                "prueba@correo.com", "cedula", "123", 1063158246);
+        listUsuarios.add(usuario);
+        System.out.println(""+usuario.getCorreo());
     }
     
+    public boolean existeUsuario(String correo, String contra){
+        for (Usuario us : listUsuarios) {
+            if(us.getCorreo().equals(correo)&&us.getPass().equals(contra)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public Usuario ObtenerUsuario(String correo, String contra){
+        for(Usuario aux: listUsuarios){
+          if(aux.getCorreo().equals(correo)&&aux.getPass().equals(contra)){
+              return aux; 
+            }   
+        }
+        return null;
+    }
 }
