d6e9f95d5b067408a43d529bf8c65c005c7f5515
Jesus Alvis <jalvisbarcenas64@correo.unicordoba.edu.co>
2024-06-23 18:38:43 -0500

    Preparando el entorno de la vista principal

--- Tienda_Virtual_Libros/src/controladores/MenuPrincipalController.java
+++ Tienda_Virtual_Libros/src/controladores/MenuPrincipalController.java
@@ -4,12 +4,31 @@
  */
 package controladores;
 
+import Utils.Utilidades;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import modelos.Usuario;
 
 public class MenuPrincipalController implements Initializable {
+
+    @FXML
+    private Label txtNombreUs;
+    @FXML
+    private Label opcInicio;
+    @FXML
+    private Label opcListaDes;
+    @FXML
+    private Label opcCarrito;
+    @FXML
+    private Label opCompras;
+    @FXML
+    private Label opcSalir;
+    @FXML
+    private Label txtApellidos;
 
     /**
      * Initializes the controller class.
@@ -17,6 +36,43 @@ public class MenuPrincipalController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+        Usuario usuarioLog = Utilidades.getInstance().getUsuario();
+        txtNombreUs.setText(usuarioLog.getNombres());
+        txtApellidos.setText(usuarioLog.getApellidos());
+    }
+
+    public void CambiarColor1(Label principal, Label s1,
+            Label s2, Label s3,
+            Label s4) {
+        principal.setStyle("-fx-text-fill: #212a65;");
+        s1.setStyle("-fx-text-fill: #c2c2d1;");
+        s2.setStyle("-fx-text-fill: #c2c2d1;");
+        s3.setStyle("-fx-text-fill: #c2c2d1;");
+        s4.setStyle("-fx-text-fill: #c2c2d1;");
+    }
+
+    @FXML
+    private void mostrarLibros(MouseEvent event) {
+        CambiarColor1(opcInicio, opcSalir, opCompras, opcCarrito, opcListaDes);
+    }
+
+    @FXML
+    private void mostrarListaD(MouseEvent event) {
+        CambiarColor1(opcListaDes, opcInicio, opcSalir, opCompras, opcCarrito);
+    }
+
+    @FXML
+    private void mostrarCarrito(MouseEvent event) {
+        CambiarColor1(opcCarrito, opcListaDes, opcInicio, opcSalir, opCompras);
+    }
+
+    @FXML
+    private void mostrarCompras(MouseEvent event) {
+        CambiarColor1(opCompras,opcCarrito, opcListaDes, opcInicio, opcSalir);
+    }
+
+    @FXML
+    private void regresar(MouseEvent event) {
+        Utilidades.getInstance().mostrarOtraVista(event, "/vistas/Login.fxml");
+    }
 }
