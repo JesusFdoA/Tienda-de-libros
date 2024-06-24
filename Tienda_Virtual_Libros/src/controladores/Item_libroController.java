7dee1d955cecfa53ba20df96af58d3877e5e5a3a
Jesus Treco <jesustrecos@gmail.com>
2024-06-24 08:38:16 -0500

    Configuracion del controlador del libro para item libro

--- Tienda_Virtual_Libros/src/controladores/Item_libroController.java
+++ Tienda_Virtual_Libros/src/controladores/Item_libroController.java
@@ -1,35 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package controladores;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import modelos.Libro;
 
-/**
- * FXML Controller class
- *
- * @author Juan Mangones
- */
-public class Item_libroController  {
+public class Item_libroController {
 
     @FXML
     private Label txtTitulo;
+
     @FXML
     private Label txtAutor;
+
     @FXML
     private Label txtPrecio;
-    
-    public void setLibro(Libro libro){
-        txtAutor.setText(libro.getAutores());
-        txtPrecio.setText(""+libro.getPrecio());
-        txtTitulo.setText(libro.getNombre());
+
+    @FXML
+    private ImageView imgPortada;
+
+    public void setTitulo(String titulo) {
+        this.txtTitulo.setText(titulo);
     }
-    
-    
+
+    public void setAutor(String autor) {
+        this.txtAutor.setText(autor);
+    }
+
+    public void setPrecio(String precio) {
+        this.txtPrecio.setText(precio);
+    }
+
+    public void setPortada(String urlImagen) {
+        this.imgPortada.setImage(new Image(urlImagen));
+    }
+
+    void setDatosLibro(Libro libro) {
+        setAutor(libro.getAutores());
+        setPrecio(""+libro.getPrecio());
+        setTitulo(libro.getNombre());
+    }
 }
