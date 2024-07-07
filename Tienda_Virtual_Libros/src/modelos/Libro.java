726b5e07bc3d8d18596ec2842a632514745cd930
Jesus Treco <jesustrecos@gmail.com>
2024-07-07 18:29:55 -0500

    Actualizacion del modelo libro

--- Tienda_Virtual_Libros/src/modelos/Libro.java
+++ Tienda_Virtual_Libros/src/modelos/Libro.java
@@ -4,13 +4,15 @@
  */
 package modelos;
 
-
 public class Libro {
+
     private String nombre, editorial, formato, presentacion, autores, categoria, isbn;
     private double precio;
     private int stock;
+    private double total;
 
     public Libro() {
+
     }
 
     public Libro(String nombre, String editorial, String formato, String presentacion, String autores, String categoria, String isbn, double precio, int stock) {
@@ -23,9 +25,8 @@ public class Libro {
         this.isbn = isbn;
         this.precio = precio;
         this.stock = stock;
+        this.total = stock * precio;
     }
-
-    
 
     public Libro(String nombre, String autores, double precio) {
         this.nombre = nombre;
@@ -47,6 +48,22 @@ public class Libro {
 
     public void setEditorial(String editorial) {
         this.editorial = editorial;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getTotal() {
+        return stock * precio;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
     }
 
     public String getFormato() {
@@ -96,10 +113,5 @@ public class Libro {
     public void setStock(int stock) {
         this.stock = stock;
     }
-    
-    
 
-   
-    
-    
 }
