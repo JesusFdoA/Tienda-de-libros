f4394c392ebf744cedf187ef93ce0b4989e8cc74
Carlos Martinez <lagnusmac@gmail.com>
2024-07-07 19:13:07 -0500

    Cambios en del controlador del menu principal

--- Tienda_Virtual_Libros/src/controladores/MenuPrincipalController.java
+++ Tienda_Virtual_Libros/src/controladores/MenuPrincipalController.java
@@ -18,6 +18,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -124,6 +125,8 @@ public class MenuPrincipalController implements Initializable {
     private Button btnCarrito;
     @FXML
     private Button btnDeseos;
+    @FXML
+    private Spinner<?> spingCant;
 
     /**
      * Initializes the controller class.
