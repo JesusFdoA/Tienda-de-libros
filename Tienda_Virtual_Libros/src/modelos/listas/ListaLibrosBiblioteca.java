47d6f3cd79c92883139f43dd3c3e3e24d4de080c
Jesus Treco <jesustrecos@gmail.com>
2024-07-07 19:48:43 -0500

    actualiazacion del modelo de libros de la biblioteca

--- Tienda_Virtual_Libros/src/modelos/listas/ListaLibrosBiblioteca.java
+++ Tienda_Virtual_Libros/src/modelos/listas/ListaLibrosBiblioteca.java
@@ -7,10 +7,6 @@ package modelos.listas;
 import java.util.ArrayList;
 import modelos.Libro;
 
-/**
- *
- * @author Juan Mangones
- */
 public class ListaLibrosBiblioteca {
 
     private ArrayList<Libro> listLibros;
@@ -20,15 +16,15 @@ public class ListaLibrosBiblioteca {
         Libro libro1 = new Libro("MÉTODOS NUMÉRICOS PARA INGENIERÍA", "ALPHA",
                 "Libro", "Tapa blanda", "ENRIQUE VILCHEZ QUESADA",
                 "Matemáticas", "9789587788914", 150000, 12);
-        
+
         Libro libro2 = new Libro("INTRODUCCIÓN A LA INGENIERÍA ESPACIAL", "ALPHA",
                 "Libro", "Tapa blanda", "DANIEL PORTNOY",
                 "Ingeniería", "9789587788792", 100000, 10);
-        
+
         Libro libro3 = new Libro("MITOS Y LEYENDAS ANTIGUO EGIPTO", "ALMA",
                 "Libro", "Tapa dura", "IAN STEWART",
                 "Mitologia", "9788419599339", 120000, 8);
-        
+
         Libro libro4 = new Libro("EVA MORDIO LA MANZANA", "SIN FRONTERAS",
                 "Libro", "Tapa blanda", "FLAVIA DOS SANTOS",
                 "Matemáticas", "9786287667723", 150000, 12);
@@ -44,5 +40,14 @@ public class ListaLibrosBiblioteca {
 
     public void setListLibros(ArrayList<Libro> listLibros) {
         this.listLibros = listLibros;
+    }
+
+    public Libro buscarPorISBN(String isbn) {
+        for (Libro libro : listLibros) {
+            if (libro.getIsbn().equals(isbn)) {
+                return libro;
+            }
+        }
+        return null;
     }
 }
